8c66ca0e9abb71211d5a9d0296ef2f0dd8278420
shenxl <shen.sxl@icloud.com>
2016-08-09 19:56:08 +0800

    Column name Convert

--- models/company.go
+++ models/company.go
@@ -1,9 +1,9 @@
 package models
 
 type Company struct {
-	id   int64
-	company string
-  sn string
+	ID   int64
+	Company string
+  Sn string
   // region string `json:"region"`
   // industry string `json:"industry"`
 }
