dc21859b9a07e2762244a94d387a3854e39c5fc7
shenxl <shen.sxl@icloud.com>
2016-08-09 19:41:18 +0800

    remove json tag

--- models/company.go
+++ models/company.go
@@ -1,9 +1,9 @@
 package models
 
 type Company struct {
-	id   int64  `json:"id"`
-	company string `json:"company"`
-  sn string `json:"sn"`
+	id   int64
+	company string
+  sn string
   // region string `json:"region"`
   // industry string `json:"industry"`
 }
